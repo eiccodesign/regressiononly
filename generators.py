75cbea46cf80bb33daac1363ec221d4ccc6bc60d
Fernando T. Torales Acosta <ftoralesacosta@ml4hep1.lbl.gov>
2023-02-11 08:54:24 -0800

    saving mean and std for scalar transform

--- generators.py
+++ generators.py
@@ -1,69 +1,36 @@
-#Processing
-import numpy as np
-import uproot as ur
-import awkward as ak
-
-#ML
-# import tensorflow as tf
-# from sklearn.preprocessing import StandardScaler
-
-#Plotting
+#Imports
 import matplotlib.pyplot as plt
-
 import numpy as np
+import glob
+import os
 import uproot as ur
 import awkward as ak
-import os
 import time
-from multiprocessing import Process, Queue, set_start_method
+from multiprocessing import Process, Queue, Manager, set_start_method
 import compress_pickle as pickle
 from scipy.stats import circmean
 import random
 
-#Simple function for defining the data generator (interface to data as if it were an infinite stream)
-#Mean and standard deviations are obtained from training dataset for performing a standard scalar transformation
+data_dir = '/clusterfs/ml4hep_nvme2/ftoralesacosta/regressiononly/data/'
+out_dir = '/clusterfs/ml4hep_nvme2/ftoralesacosta/regressiononly/preprocessed_data/'
 
-scales = {
-    'cell_E_mean': 0.0,
-    'cell_E_std': 0.0,
-    'cell_X_mean': 0.0,
-    'cell_X_std': 0.0,
-    'cell_Y_mean': 0.0,
-    'cell_Y_std': 0.0,
-    'cell_Z_mean': 0.0,
-    'cell_Z_std': 0.0}
 
-class generator:
-    def __init__(self, file, dataset):
-        self.file = file
-        self.dataset = dataset
-
-    def __call__(self):
-        with h5py.File(self.file, 'r') as hf:
-            for data in hf[self.dataset]:
-                yield data
-
-class data_generator:
+class MPGraphDataGenerator:
     def __init__(self,
                  file_list: list,
                  batch_size: int,
                  shuffle: bool = True,
                  num_procs = 32,
+                 calc_scalar= False,
                  preprocess = False,
-                 output_dir = None,
-                 take_log10 = False):
+                 output_dir = None):
         """Initialization"""
 
         self.preprocess = preprocess
         self.output_dir = output_dir
 
-        if self.preprocess and self.output_dir is not None:
-            self.file_list = file_list
-            self.num_files = len(self.file_list)
-
-        else:
-            self.file_list = file_list
-            self.num_files = len(self.file_list)
+        self.file_list = file_list
+        self.num_files = len(self.file_list)
 
         self.batch_size = batch_size
         self.shuffle = shuffle
@@ -73,22 +40,111 @@ class data_generator:
         self.num_procs = num_procs
         self.procs = []
 
-        if self.preprocess and self.output_dir is not None:
-            os.makedirs(self.output_dir, exist_ok=True)
-            self.preprocess_data()
 
         self.detector_name = "HcalEndcapPHitsReco" #'Insert' after the 'P'
         self.sampling_fraction = 0.02 #0.0098 for Insert
-        self.take_log10 = take_log10
 
         self.nodeFeatureNames = [".energy",".position.x",".position.y",".position.z",]
         self.num_nodeFeatures = len(self.nodeFeatureNames)
-        self.meta_features = ['file_name', 'event_ind']
+        self.num_targetFeatures = 1 #Regression on Energy only for now
+
         # self.edgeFeatureNames = self.cellGeo_data.keys()[9:]
         # self.num_edgeFeatures = len(self.edgeFeatureNames)
 
+        if self.preprocess and self.output_dir is not None:
+            os.makedirs(self.output_dir, exist_ok=True)
+            n_scalar_files = 8 #num files to use for scaler calculation
+            self.preprocess_scalar(n_scalar_files)
+            self.preprocess_data()
+
+
+
+    def preprocess_scalar(self,n_calcs):
+        print(f'\nCalcing Scalars and saving data to {self.output_dir}')
+
+        self.n_calcs = min(n_calcs,self.num_files)
+
+        with Manager() as manager:
+            means = manager.list()
+            stdvs = manager.list()
+
+            for i in range(self.n_calcs):
+                p = Process(target=self.scalar_processor,
+                            args=(i,means,stdvs), daemon=True)
+                p.start()
+                self.procs.append(p)
+
+            for p in self.procs:
+                p.join()
+
+            means = np.mean(means,axis=0) #avg means along file dimension
+            stdvs = np.mean(stdvs,axis=0) #avg stdvs from files
+            print("MEANS = ",means)
+            print("STDVS = ",stdvs)
+
+            pickle.dump(means, open(
+                        self.output_dir + 'means.p', 'wb'), 
+                    compression='gzip')
+
+            pickle.dump(stdvs, open(
+                        self.output_dir + 'stdvs.p', 'wb'), 
+                    compression='gzip')
+
+        print(f"Finished Mean and Standard Deviation Calculation using { n_calcs } Files")
+
+
+    def scalar_processor(self,worker_id,means,stdvs):
+
+        file_num = worker_id
+
+        while file_num < self.num_files:
+            print(f"Mean + Stdev Calc. file number {file_num}")
+            f_name = self.file_list[file_num]
+
+            event_tree = ur.open(f_name)['events']
+            num_events = event_tree.num_entries
+            event_data = event_tree.arrays() #need to use awkward
+
+            file_means = []
+            file_stdvs = []
+
+            cell_E = event_data[self.detector_name+".energy"]
+            mask = cell_E > 0.0
+
+            for feature_name in self.nodeFeatureNames:
+                feature_data = event_data[self.detector_name+feature_name][mask]
+
+                if "energy" in feature_name:  
+                    feature_data = np.log10(feature_data)
+
+                file_means.append(ak.mean(feature_data))
+                file_stdvs.append(ak.std(feature_data))
+                #unfortunatley, there's a version error so we can't use ak.nanmean...
+
+            cluster_sum_E = ak.sum(cell_E,axis=-1) #global node feature later
+            mask = cluster_sum_E > 0.0
+            cluster_calib_E  = np.log10(cluster_sum_E[mask] / self.sampling_fraction)
+            file_means.append(np.nanmean(cluster_calib_E))
+            file_stdvs.append(np.nanstd(cluster_calib_E))
+            
+            genPx = event_data['MCParticles.momentum.x'][:,2]
+            genPy = event_data['MCParticles.momentum.y'][:,2]
+            genPz = event_data['MCParticles.momentum.z'][:,2]
+            genP = np.log10(np.sqrt(genPx*genPx + genPy*genPy + genPz*genPz))
+            #generation has the parent particle at index 2
+
+            file_means.append(ak.mean(genP))
+            file_stdvs.append(ak.std(genP))
+
+            means.append(file_means)
+            stdvs.append(file_stdvs)
+
+            file_num += self.num_procs
+
+
     def preprocess_data(self):
-        print('\nPreprocessing and saving data to {}'.format(self.output_dir))
+        print(f'\nPreprocessing and saving data to {self.output_dir}')
+
         for i in range(self.num_procs):
             p = Process(target=self.preprocessor, args=(i,), daemon=True)
             p.start()
@@ -104,7 +160,7 @@ class data_generator:
         file_num = worker_id
 
         while file_num < self.num_files:
-            print(f"Proceesing file number {file_num}")
+            print(f"Processing file number {file_num}")
             f_name = self.file_list[file_num]
 
             event_tree = ur.open(f_name)['events']
@@ -114,22 +170,23 @@ class data_generator:
             preprocessed_data = []
 
             for event_ind in range(num_events):
-                
-                #FIXME: may want to pass cluster_sum as only node at first.
 
-                nodes, global_node, cluster_num_nodes = self..get_nodes(event_data, event_ind)
-                senders, receivers, edges = self.get_edges(cluster_num_nodes)
+                nodes, global_node, cluster_num_nodes = self.get_nodes(event_data, event_ind)
+                senders, receivers, edges = self.get_edges(cluster_num_nodes) #returns 'None'
 
                 graph = {'nodes': nodes.astype(np.float32), 'globals': global_node.astype(np.float32),
-                    'senders': senders.astype(np.int32), 'receivers': receivers.astype(np.int32),
-                    'edges': edges.astype(np.float32)}
+                    'senders': senders, 'receivers': receivers, 'edges': edges} 
 
-                target = get_GenP(event_data)
+                # graph = {'nodes': nodes.astype(np.float32), 'globals': global_node.astype(np.float32),
+                #     'senders': senders.astype(np.int32), 'receivers': receivers.astype(np.int32),
+                #     'edges': edges.astype(np.float32)}
+
+                target = self.get_GenP(event_data,event_ind)
 
                 meta_data = [f_name]
                 meta_data.extend(self.get_meta(event_data, event_ind))
 
-                preprocessed_data.append((graph, target))
+                preprocessed_data.append((graph, target,meta_data))
 
             random.shuffle(preprocessed_data) #should be done BEFORE multiple 'images' per geant event
 
@@ -141,22 +198,30 @@ class data_generator:
             print(f"Finished processing {file_num} files")
 
 
-    def get_nodes(self):
 
-        nodes = get_cell_data(event_data)
+    def get_nodes(self,event_data,event_ind):
+
+        nodes = self.get_cell_data(event_data[event_ind])
         cluster_num_nodes = len(nodes)
-        global_node = get_cluster_calib(event_data)
+        global_node = self.get_cluster_calib(event_data[event_ind])
 
         return nodes, np.array([global_node]), cluster_num_nodes
 
-
-    def get_cell_data(event_data):
+    def get_cell_data(self,event_data):
 
         cell_data = []
-        for nodeFeatureName in self.nodeFeatureNames:
-            cell_data.append(event_data[self.detector_name+nodeFeatureName])
 
-        return np.swapaxes(cell_data,0,1) #Events, Features
+        cell_E = event_data[self.detector_name+".energy"]
+        mask = cell_E > 0.0
+
+        for feature in self.nodeFeatureNames:
+
+            if "energy" in feature:  
+                cell_data.append(np.log10(event_data[self.detector_name+feature][mask]))
+            else:
+                cell_data.append(event_data[self.detector_name+feature][mask])
+
+        return np.swapaxes(cell_data,0,1) # returns [Events, Features]
     #alternative: cell_data = np.reshape(cell_data, (len(self.nodeFeatureNames), -1)).T
 
 
@@ -165,27 +230,46 @@ class data_generator:
 
         cell_E = event_data[self.detector_name+".energy"]
         cluster_sum_E = np.sum(cell_E,axis=-1) #global node feature later
-        cluster_calib_E  = cluster_E / self.sampling_fraction
+        cluster_calib_E  = cluster_sum_E / self.sampling_fraction
 
         if cluster_calib_E <= 0:
             return None
 
-        if (take_log10):
-            return np.log10(cluster_calib_E)
         else:
             return(cluster_calib_E)
 
     def get_edges(self, num_nodes):
         return None,None,None
 
-    def get_GenP(self,event_data):
-        
-        genPx = event_data['MCParticles.momentum.x'][:,2]
-        genPy = event_data['MCParticles.momentum.y'][:,2]
-        genPz = event_data['MCParticles.momentum.z'][:,2]
+    def get_GenP(self,event_data,event_ind):
 
-        genP = np.sqrt(genPx*genPx + genPy*genPy + genPz*genPz)
+        genPx = event_data['MCParticles.momentum.x'][event_ind,2]
+        genPy = event_data['MCParticles.momentum.y'][event_ind,2]
+        genPz = event_data['MCParticles.momentum.z'][event_ind,2]
+        #the generation has the parent praticle always at index 2
+
+        genP = np.log10(np.sqrt(genPx*genPx + genPy*genPy + genPz*genPz))
         return genP
+
+
+
+    #FIXME: DELETE THIS AND TARGET SCALARS
+    def get_cell_scalars(self,event_data):
+
+        means = []
+        stdvs = []
+
+        for feature in nodeFeatureNames:
+            means.append(np.nanmean(event_data[feature]))
+            stdvs.append(np.nanstd( event_data[feature]))
+
+        return means, stdvs
+
+
+    def get_target_scalars(self,target):
+
+        return np.nanmean(target), np.nanstd(target)
+
 
     def get_meta(self, event_data, event_ind):
         """ 
@@ -195,10 +279,9 @@ class data_generator:
         #For Now, only holds event id. Only one cluster per event, and no eta/phi
         meta_data = [] 
         meta_data.append(event_ind)
-        
+
         return meta_data
 
-    #FIXME: rm meta data
     def preprocessed_worker(self, worker_id, batch_queue):
         batch_graphs = []
         batch_targets = []
@@ -208,37 +291,41 @@ class data_generator:
         while file_num < self.num_files:
             file_data = pickle.load(open(self.file_list[file_num], 'rb'), compression='gzip')
 
+            print("FILE DATA SHAPE = ",np.shape(file_data))
+
             for i in range(len(file_data)):
                 batch_graphs.append(file_data[i][0])
                 batch_targets.append(file_data[i][1])
                 batch_meta.append(file_data[i][2])
-                    
+
+                # batch_targets = np.reshape(np.array(batch_targets), [-1,2]).astype(np.float32)
+                '''need the above line if there are more than 1 cluster per event'''
+
                 if len(batch_graphs) == self.batch_size:
-                    batch_targets = np.reshape(np.array(batch_targets), [-1,2]).astype(np.float32)
-                    
+
                     batch_queue.put((batch_graphs, batch_targets, batch_meta))
-                    
+
                     batch_graphs = []
                     batch_targets = []
                     batch_meta = []
 
             file_num += self.num_procs
-                    
+
         if len(batch_graphs) > 0:
-            batch_targets = np.reshape(np.array(batch_targets), [-1,2]).astype(np.float32)
-            
+            # batch_targets = np.reshape(np.array(batch_targets), [-1,2]).astype(np.float32)
+
             batch_queue.put((batch_graphs, batch_targets, batch_meta))
 
     def worker(self, worker_id, batch_queue):
         if self.preprocess:
             self.preprocessed_worker(worker_id, batch_queue)
         else:
-            raise Exception('Preprocessing is required for combined classification/regression models.')
-        
+            raise Exception('Preprocessing is required for regression models.')
+
     def check_procs(self):
         for p in self.procs:
             if p.is_alive(): return True
-        
+
         return False
 
     def kill_procs(self):
@@ -249,43 +336,43 @@ class data_generator:
 
 
     def generator(self):
-        # for file in self.file_list:
         batch_queue = Queue(2 * self.num_procs)
-            
+
         for i in range(self.num_procs):
             p = Process(target=self.worker, args=(i, batch_queue), daemon=True)
             p.start()
             self.procs.append(p)
-        
+
         while self.check_procs() or not batch_queue.empty():
             try:
                 batch = batch_queue.get(True, 0.0001)
             except:
                 continue
-            
+
+            #FIXME: Print Batches here too
             yield batch
-        
+
         for p in self.procs:
             p.join()
-            
+
 if __name__ == '__main__':
-    data_dir = '/usr/workspace/pierfied/preprocessed/data/'
-    out_dir = '/usr/workspace/pierfied/preprocessed/preprocessed_data/'
-    pion_files = np.sort(glob.glob(data_dir+'user*.root'))
-    
+    pion_files = np.sort(glob.glob(data_dir+'*.root')) #dirs L14
+    pion_files = pion_files[:8]
+    print("Pion Files = ",pion_files)
+
     data_gen = MPGraphDataGenerator(file_list=pion_files, 
-                                  cellGeo_file=data_dir+'cell_geo.root',
-                                  batch_size=32,
-                                  shuffle=False,
-                                  num_procs=32,
-                                  preprocess=True,
-                                  output_dir=out_dir)
+                                    batch_size=32,
+                                    shuffle=False,
+                                    num_procs=32,
+                                    preprocess=True,
+                                    output_dir=out_dir)
 
     gen = data_gen.generator()
-    
+
     from tqdm.auto import tqdm
-     
+
     for batch in tqdm(gen):
         pass
-        
+
+    print("\n~ DONE ~\n")
     exit()
